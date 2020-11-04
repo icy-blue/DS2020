8cf69ec69c38900d9b797738e8caf7d8905a6f10
icy <i@icys.top>
2020-11-04 10:58:02 +0800

    fenwick init

---
+++ FenwickTree.h
@@ -0,0 +1,15 @@
+/**
+  * Created by Huang Jingcheng on 2020/11/4.
+  * 201905130198@mail.sdu.edu.cn
+*/
+
+#ifndef DS2020_FENWICKTREE_H
+#define DS2020_FENWICKTREE_H
+
+
+class FenwickTree {
+
+};
+
+
+#endif //DS2020_FENWICKTREE_H
