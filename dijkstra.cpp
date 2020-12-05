1b2461ead5c9621e94ff474f9ea5a94b42c8d5ed
icy-blue <i@icys.top>
2020-12-06 00:59:30 +0800

    dijkstra

---
+++ dijkstra.cpp
@@ -0,0 +1,91 @@
+/**
+ * Created by Clion.
+ * User: Huang Jingcheng
+ * Date: 2020/12/5
+ * 201905130198@mail.sdu.edu.cn
+ */
+
+#include "dijkstra.h"
+#include <vector>
+#include <queue>
+#include <cassert>
+#include <stack>
+#include <optional>
+
+namespace DS2020 {
+#define TYPE_DISTANCE double
+#define DISTANCE_MAX 1e10
+#define DISTANCE_ZERO 0
+
+    class Node;
+
+    class Edge {
+    public:
+        Node *from, *to;
+        TYPE_DISTANCE distance;
+
+        Edge(Node *_from, Node *_to, TYPE_DISTANCE _distance) : from(_from), to(_to), distance(_distance) {}
+    };
+
+    class Node {
+    public:
+        int id; //from 0 to size(nodeList) - 1
+        std::vector<Edge> edges;
+
+        bool operator<(const Node &rt) const {
+            return this->id < rt.id;
+        }
+    };
+
+    std::vector<TYPE_DISTANCE> distance;
+    std::vector<Node *> trace;
+    std::vector<Node> nodeList;
+
+    void dijkstra(std::vector<Node> &nodes, Node &source) {
+        assert(!nodes.empty());
+        distance.clear();
+        distance.resize(nodes.size(), DISTANCE_MAX);
+        trace.clear();
+        trace.resize(nodes.size(), nullptr);
+        std::vector<bool> visit(nodes.size());
+        std::priority_queue<std::pair<TYPE_DISTANCE, Node>> queue;
+        visit[source.id] = true;
+        distance[source.id] = DISTANCE_ZERO;
+        queue.push(std::make_pair(DISTANCE_ZERO, source));
+        while (!queue.empty()) {
+            TYPE_DISTANCE disNow = queue.top().first;
+            Node node = queue.top().second;
+            queue.pop();
+            if (visit[node].id) continue;
+            visit[node.id] = true;
+            for (auto edge: node.edges) {
+                if (distance[edge.to->id] < disNow + edge.distance) {
+                    distance[edge.to->id] = disNow + edge.distance;
+                    queue.push(std::make_pair(distance[edge.to->id], *edge.to));
+                    trace[edge.to->id] = &node;
+                }
+            }
+        }
+    }
+
+    std::optional<std::stack<Node *>> traceRoute(Node &source, Node &destination) {
+        assert(distance[source.id] == DISTANCE_ZERO);
+        std::stack<Node *> stack;
+        for (Node *now = &destination; now != &source; now = trace[now->id]) {
+            if (now == nullptr) {
+                return std::nullopt;
+            }
+            stack.push(now);
+        }
+        stack.push(&source);
+        return stack;
+    }
+
+    void addEdgeFromID(int from, int to, TYPE_DISTANCE dis) {
+        nodeList[from].edges.emplace_back(&nodeList[from], &nodeList[to], dis);
+    }
+
+    void addEdgeFromNode(Node *from, Node *to, TYPE_DISTANCE dis) {
+        nodeList[from->id].edges.emplace_back(from, to, dis);
+    }
+}
---
+++ dijkstra.h
@@ -0,0 +1,11 @@
+/**
+ * Created by Clion.
+ * User: Huang Jingcheng
+ * Date: 2020/12/5
+ * 201905130198@mail.sdu.edu.cn
+ */
+
+#ifndef DS2020_DIJKSTRA_H
+#define DS2020_DIJKSTRA_H
+
+#endif //DS2020_DIJKSTRA_H
