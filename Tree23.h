933c865f45876ee2ab9b573825b165d0b65fa81b
icy-blue <i@icys.top>
2020-12-07 00:37:41 +0800

    tree23

--- Tree23.h
+++ Tree23.h
@@ -11,15 +11,45 @@
 #include <vector>
 #include <functional>
 #include <algorithm>
+#include <cassert>
 
 template<typename T, typename Container = std::vector<T>, typename less = std::less<typename Container::value_type>>
 class Tree23 {
+#define SIZE_DATA 2
+
     class Node {
     public:
-        Node *leftSon, *midSon, *rightSon, *father;
-        T data1, data2;
+        Node *sons[SIZE_DATA + 1], *father;
+        T data[SIZE_DATA];
         int nodeSize = 1, treeSize = 1;
         bool isLeaf = true;
+    };
+
+    class TmpNode {
+    public:
+        Node *sons[SIZE_DATA + 2], *father;
+        T data[SIZE_DATA + 1];
+        int nodeSize = 1, treeSize = 1;
+        bool isLeaf = true;
+
+        explicit TmpNode(const Node &node) {
+            for (auto i = 0; i < node.nodeSize; i++) {
+                this->sons[i] = node.sons[i];
+                this->data[i] = node.data[i];
+            }
+            this->sons[node.nodeSize] = node.sons[node.nodeSize];
+            this->father = node.father;
+            this->nodeSize = node.nodeSize;
+            this->treeSize = node.treeSize;
+            this->isLeaf = node.isLeaf;
+        }
+
+        TmpNode() = default;
+
+        TmpNode operator+(const Node &node) {
+            auto origin = TmpNode(node);
+
+        }
     };
 
 public:
@@ -34,42 +64,52 @@ public:
             return;
         }
         auto node = root;
-        T tmpData;
+        TmpNode *tmpNode;
         auto needUpdate = false;
-        while (node != nullptr) {
+        while (true) {
+            assert(node != nullptr);
             if (node->isLeaf) {
                 if (node->nodeSize == 1) {
-                    node->data2 = node->data1;
-                    if (less(node->data2, node->data1)) {
-                        std::swap(node->data1, node->data2);
+                    node->data[1] = data;
+                    if (less(node->data[0], node->data[1])) {
+                        std::swap(node->data[0], node->data[1]);
                     }
                     node->nodeSize++, node->treeSize++;
                 } else {
-                    T insertArray[3] = {node->data1, node->data2, data};
+                    T insertArray[3] = {node->data[0], node->data[1], data};
                     std::sort(insertArray, insertArray + 3);
-                    tmpData = insertArray[1];
+                    Node *leftSon = new Node, *rightSon = new Node;
+                    tmpNode = new TmpNode;
+                    leftSon->father = rightSon->father = tmpNode;
+                    leftSon->data[0] = insertArray[0];
+                    rightSon->data[0] = insertArray[2];
+                    tmpNode->data[0] = insertArray[1];
+                    tmpNode->sons[0] = leftSon;
+                    tmpNode->sons[1] = rightSon;
+                    tmpNode->isLeaf = false;
                     needUpdate = true;
+                    break;
                 }
             } else {
-
+                auto i = 0;
+                while (i < node->nodeSize && less(data, node->data[i]) {
+                    i++;
+                }
+                node = node->sons[i];
             }
-//            if (less(data, node->data1)) {
-
-//                } else node = node->leftSon;
-//            } else if (node->nodeSize == 1) {
-//                if (node->isLeaf) {
-//                    node->data2 = data;
-//                } else {
-//                    node = node->midSon;
-//                }
-//            } else if (less(data, node->data2)) {
-//                if (node->isLeaf) {
-//                    tmpData = data;
-//                    needUpdate = true;
-//                } else {
-//
-//                }
-//            }
+        }
+        while (needUpdate) {
+            needUpdate = false;
+            auto i = 0;
+            while (i < node->father->nodeSize && less(node->father->data[i], tmpData) {
+                i++;
+            }
+            tmpNode.sons[SIZE_DATA + 1] = tmpNode.sons[SIZE_DATA];
+            for (int j = node->nodeSize - 1; j >= i; j++) {
+                tmpNode.sons[j + 1] = tmpNode.sons[j];
+                tmpNode.data[j + 1] = tmpNode.data[j];
+            }
+            tmpNode.sons[i] =
         }
     }
 };
