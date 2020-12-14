383afb61913c04d45f60da655a07eb2b19ad7efd
icy <i@icys.top>
2020-12-14 15:11:00 +0800

    tree23

--- Tree23-2.h
+++ Tree23-2.h
@@ -92,16 +92,142 @@ public:
         }
     }
 
+private:
+    Node *getSibling(Node *node) {
+        if (node == nullptr or node->father == nullptr)return nullptr;
+        assert(node->father->nodeSize == 1);
+        if (node == node->father->sons[0]) return node->father->sons[1];
+        return node->father->sons[0];
+    }
+
+public:
+    void del(const T &data) {
+        Node *node = find(data);
+        if (node == nullptr)return;
+        Node *tmp = root;
+        auto checkSingle = [](Node *node) -> bool {
+            return node == nullptr or node->nodeSize == 1;
+        };
+        while (tmp != nullptr) {
+            if (checkSingle(tmp)) {
+                if (less(data, tmp->data[0]) and checkSingle(tmp->sons[0])) {
+                    Node *sibling = getSibling(tmp);
+                    if (checkSingle(sibling)) {
+                        if (less(sibling->data[0], tmp->data[0])) {
+                            std::swap(sibling, tmp);
+                        }
+                        int i = 0;
+                        while (less(tmp->father->data[i], data))i++;
+                        tmp->data.push_back(tmp->father->data[i]);
+                        tmp->father->data.erase(tmp->father->data.begin() + i);
+                        tmp->father->nodeSize--;
+                        int j = 0;
+                        while (node->father->sons[j] != sibling) j++;
+                        node->father->sons.erase(node->father->sons.begin() + j);
+                        tmp->data.push_back(sibling->data[0]);
+                        tmp->sons.push_back(sibling->sons[0]);
+                        tmp->sons.push_back(sibling->sons[1]);
+                        delete sibling;
+                        for (auto item:tmp->sons) {
+                            item->father = tmp;
+                        }
+                        if (tmp->father->nodeSize == 0) {
+                            tmp->father = tmp->father->father;
+                            if (tmp->father->father != nullptr) {
+                                for (auto item:tmp->father->father->sons) {
+                                    if (item == node->father) item = tmp;
+                                }
+                            } else {
+                                root = tmp;
+                            }
+                            delete tmp->father;
+                        }
+                    }
+                }
+            }
+            int i = 0;
+            while (less(tmp->data[i], data) and !less(data, tmp->data[i])) i++;
+            if (!(less(tmp->data[i], data) xor less(data, tmp->data[i]))) {
+                if (tmp.isLeaf) {
+                    tmp.data.erase(tmp.data.begin() + i);
+                } else {
+                    Node *after = findNext(tmp, data);
+                    assert(after != nullptr);
+                    tmp->data[i] = after->data[0];
+                    after->data[0] = data;
+                }
+            }
+
+        }
+    }
+
+    Node *find(const T &data) {
+        if (root == nullptr) return nullptr;
+        Node *tmp = root;
+        while (tmp != nullptr) {
+            int i = 0;
+            while (less(tmp->data[i], data) and !less(data, tmp->data[i])) i++;
+            if (!(less(tmp->data[i], data) xor less(data, tmp->data[i]))) return tmp;
+            tmp = tmp->sons[i];
+        }
+        return tmp;
+    }
+
     Node *findNext(Node *node, const T &data) const {
+        assert(node != nullptr);
         Node *tmp = node;
-        if (node != nullptr and node->rightSon != nullptr)return findMin(node->rightSon);
+        int i = 0;
+        while (i < node->nodeSize and less(node->data[i], data) and !less(data, node->data[i])) i++;
+        if (!node->isLeaf)return findMin(node->sons[i + 1]);
+        if (i < node->nodeSize) return node;
         while (tmp != nullptr) {
             if (tmp->father == nullptr)return nullptr;
-            if (cmp(tmp->data, tmp->father->data)) {
-                return tmp->father;
-            } else {
-                tmp = tmp->father;
+            i = 0;
+            while (i < node->father->nodeSize and less(node->father->data[i], data)
+                   and !less(data, node->data[i])) {
+                i++;
             }
+            if (i < node->father->nodeSize) return findMin(node->father->sons[i + 1]);
+            tmp = tmp->father;
+        }
+        return tmp;
+    }
+
+    Node *findPrev(Node *node, const T &data) const {
+        assert(node != nullptr);
+        Node *tmp = node;
+        int i = 0;
+        while (i < node->nodeSize and less(node->data[i], data) and !less(data, node->data[i])) i++;
+        if (!node->isLeaf)return findMax(node->sons[i]);
+        if (i > 0) return node;
+        while (tmp != nullptr) {
+            if (tmp->father == nullptr)return nullptr;
+            i = 0;
+            while (i < node->father->nodeSize and less(node->father->data[i], data)
+                   and !less(data, node->data[i])) {
+                i++;
+            }
+            if (i > 0) return findMax(node->father->sons[i]);
+        }
+        return tmp;
+    }
+
+    Node *findMin(Node *node) const {
+        assert(node != nullptr);
+        Node *tmp = node;
+        while (!tmp->isLeaf) {
+            assert(tmp != nullptr);
+            tmp = tmp->sons[0];
+        }
+        return tmp;
+    }
+
+    Node *findMax(Node *node) const {
+        assert(node != nullptr);
+        Node *tmp = node;
+        while (!tmp->isLeaf) {
+            assert(tmp != nullptr);
+            tmp = tmp->sons[tmp->nodeSize];
         }
         return tmp;
     }
