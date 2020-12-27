8a01d2e6f22e321472283378bb735dc7f7b1610d
icy <i@icys.top>
2020-12-27 14:45:27 +0800

    opengl

---
+++ src/Line.h
@@ -0,0 +1,14 @@
+/**
+ * Created by Clion.
+ * User: Huang Jingcheng
+ * Date: 2020/12/24
+ * 201905130198@mail.sdu.edu.cn
+ */
+
+
+#ifndef DS2020_LINE_H
+#define DS2020_LINE_H
+
+#include <GL/glut.h>
+
+#endif //DS2020_LINE_H
